c08ea72a41a751e2dd003ed3d5b8b086babb5934
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-12 19:13:29 -0400

    add assignment3

---
+++ a3/a3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace a3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The output is: " + (num1 * 50));
+
+            Console.Write("Please enter an integer: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The output is: " + (num2 + 25));
+
+            Console.Write("Please enter a number: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The output is: " + (num3 / 12.5));
+
+            Console.Write("Please enter an integer: ");
+            bool num4 = Convert.ToInt32(Console.ReadLine()) > 50;
+            Console.WriteLine("The output is: " + num4);
+
+            Console.Write("Please enter an integer: ");
+            int num5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The output is: " + (num5 % 25));
+
+            Console.ReadLine();
+
+        }
+    }
+}
