b224fd57426e510e880ca3372187ee3679a46033
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-12 18:50:24 -0400

    add assignment2

---
+++ assignment1/assignment1/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace assignment1
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ assignment2/assignment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int page_number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            bool need_help = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string pos_exp = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
