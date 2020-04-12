2fe4e0f18f586f4ac073e9fdcf891d293190aca0
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-12 19:47:44 -0400

    add assignment5

---
+++ a5/a5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace a5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && dui == false && tickets <= 3);
+
+            Console.ReadLine();
+        }
+    }
+}
