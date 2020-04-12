bb4abffc6368e6deb52a05a58c34b0d5fa972669
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-12 18:28:24 -0400

    add assignment1

---
+++ assignment1/assignment1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.Read();
+        }
+    }
+}
