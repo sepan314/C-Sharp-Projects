8af0d0dcd727212139f1094d164d70754446fb13
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-13 19:01:50 -0400

    assignment7

---
+++ a8/a8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace a8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("I " + "want " + "ice cream!");
+            Console.WriteLine("upper".ToUpper());
+
+            StringBuilder s = new StringBuilder();
+
+            s.Append("Twinkle, twinkle, little star, \n");
+            s.Append("How I wonder what you are!\n");
+            s.Append("Up above the world so high,\n");
+            s.Append("Like a diamond in the sky.\n");
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+    }
+}
+
