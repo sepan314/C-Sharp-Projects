c9b17e11cbd463398058f040ec9399376de5ac9e
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-13 18:40:32 -0400

    assignment7

---
+++ a7/a7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace a7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("While Loop...");
+            bool condition = true;
+            int counter = 1;
+
+            while (condition)
+            {
+                Console.WriteLine(counter);
+                counter++;
+                if (counter > 5)
+                {
+                    condition = false;
+                }
+
+            }
+
+            condition = true;
+            counter = 1;
+
+            Console.WriteLine("Do/While Loop...");
+            do
+            {
+                Console.WriteLine(counter);
+                counter = counter * 5;
+                if (counter > 125)
+                {
+                    condition = false;
+                }
+            }
+            while (condition);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
