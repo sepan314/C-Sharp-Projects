18cef8ba94dd7d378e5c5a0dcac4cb6b79c73e65
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-14 17:10:22 -0400

    assignment10 fixed

--- a10/a10/Program.cs
+++ a10/a10/Program.cs
@@ -7,20 +7,20 @@ namespace a10
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("1.");
+            Console.WriteLine("     1.");
 
-            //string[] text = { "a", "b", "c", "d", "e" };
-            //Console.WriteLine("Please enter a string: ");
-            //string str = Console.ReadLine();
-            //for (int i = 0; i < text.Length; i++)
-            //{
-            //    text[i] += str;
-            //}
+            string[] text = { "a", "b", "c", "d", "e" };
+            Console.WriteLine("Please enter a string: ");
+            string str = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] += str;
+            }
 
-            //foreach (string i in text)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            foreach (string i in text)
+            {
+                Console.WriteLine(i);
+            }
 
             //Console.WriteLine("2.");
             //for (int i = 0; i >= 0; i++)
@@ -28,65 +28,65 @@ namespace a10
             //    Console.WriteLine(i);
             //}
 
-            //Console.WriteLine("3.");
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            Console.WriteLine("     3.");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
 
-            //Console.WriteLine("4.");
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            Console.WriteLine("     4.");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
 
-            //Console.WriteLine("5.");
-            //for (int i = 0; i <= 20; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            Console.WriteLine("     5.");
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
 
-            //Console.WriteLine("6.");
-            //List<string> str_list = new List<string>() { "mon", "tues", "wed", "thurs", "fri" };
-            //Console.WriteLine("Please enter a string: ");
-            //string str = Console.ReadLine();
-            //bool str_exist = false;
-            //for (int i = 0; i < str_list.Count; i++)
-            //{
-            //    if (str_list[i] == str)
-            //    {
-            //        str_exist = true;
-            //        Console.WriteLine(i);
-            //        break;
+            Console.WriteLine("     6.");
+            List<string> str_list = new List<string>() { "mon", "tues", "wed", "thurs", "fri" };
+            Console.WriteLine("Please enter a string: ");
+            string str1 = Console.ReadLine();
+            bool str_exist = false;
+            for (int i = 0; i < str_list.Count; i++)
+            {
+                if (str_list[i] == str1)
+                {
+                    str_exist = true;
+                    Console.WriteLine(i);
+                    break;
 
-            //    }
-            //}
-            //if (!str_exist)
-            //{
-            //    Console.WriteLine("Your string is not in list.");
+                }
+            }
+            if (!str_exist)
+            {
+                Console.WriteLine("Your string is not in list.");
 
-            //}
+            }
 
-            //Console.WriteLine("7.");
-            //List<string> str_list2 = new List<string>() { "jan", "feb", "mar", "apr", "apr" };
-            //Console.WriteLine("Please enter a string: ");
-            //string str2 = Console.ReadLine();
-            //bool str_exist2 = false;
-            //for (int i = 0; i < str_list2.Count; i++)
-            //{
-            //    if (str_list2[i] == str2)
-            //    {
-            //        str_exist2 = true;
-            //        Console.WriteLine(i);
-            //    }
-            //}
-            //if (!str_exist2)
-            //{
-            //    Console.WriteLine("Your string is not in list.");
+            Console.WriteLine("     9.");
+            List<string> str_list2 = new List<string>() { "jan", "feb", "mar", "apr", "apr" };
+            Console.WriteLine("Please enter a string: ");
+            string str2 = Console.ReadLine();
+            bool str_exist2 = false;
+            for (int i = 0; i < str_list2.Count; i++)
+            {
+                if (str_list2[i] == str2)
+                {
+                    str_exist2 = true;
+                    Console.WriteLine(i);
+                }
+            }
+            if (!str_exist2)
+            {
+                Console.WriteLine("Your string is not in list.");
 
-            //}
+            }
 
-            Console.WriteLine("7.");
+            Console.WriteLine("     11.");
             List<string> str_list3 = new List<string>() { "jan", "feb", "mar", "apr", "apr" };
             List<string> str_new = new List<string>();
             string msg;
@@ -108,8 +108,6 @@ namespace a10
 
             }
 
-
-            //str_list.IndexOf(str);
 
 
             Console.ReadLine();
