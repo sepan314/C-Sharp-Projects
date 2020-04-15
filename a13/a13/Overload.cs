286abcf6a38c05fb02480499287db81f8653c678
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-15 16:02:17 -0400

    assignment13

---
+++ a13/a13/Overload.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace a13
+{
+    public class Overload
+    {
+        public int Method(int integer)
+        {
+            return integer *= 10;
+        }
+
+        public int Method(decimal dec)
+        {
+            return Convert.ToInt32(dec /= 2);
+        }
+
+        public int Method(string str)
+        {
+            return Convert.ToInt32(str) + 100;
+        }
+
+
+    }
+}
---
+++ a13/a13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace a13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Overload s1 = new Overload();
+            Console.WriteLine(s1.Method(3));
+
+            Overload s2 = new Overload();
+            Console.WriteLine(s2.Method(5.5m));
+
+            Overload s3 = new Overload();
+            Console.WriteLine(s3.Method("20"));
+
+            Console.ReadLine();
+        }
+    }
+}
