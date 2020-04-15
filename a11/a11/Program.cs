c3640508e3811f1a0b88ff5275ab1520ce4994b1
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-15 14:58:15 -0400

    assignment11

---
+++ a11/a11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace a11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> int_list = new List<int>() { 4, 10, 20 };
+                Console.WriteLine("Please enter an integer: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < int_list.Count; i++)
+                {
+                    int_list[i] = int_list[i] / input;
+                    Console.WriteLine(int_list[i]);
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.WriteLine("Continued...");
+            Console.ReadLine();
+
+        }
+    }
+}
