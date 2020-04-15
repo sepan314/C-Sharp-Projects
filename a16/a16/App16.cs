12b2655b3bcc363a060672fbe1874bebd126b74c
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-15 17:13:40 -0400

    assignment16

---
+++ a16/a16/App16.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace a16
+{
+    public class App16
+    {
+        public void Method1(int int1)
+        {
+            Console.WriteLine(int1 / 2);
+        }
+
+        public void Method1()
+        {
+            Console.WriteLine("Boo ya");
+        }
+
+        public void Method2(out int a, out int b)
+        {
+            a = 25;
+            b = 50;
+        }
+
+
+    }
+
+}
---
+++ a16/a16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace a16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App16 obj = new App16();
+            Console.WriteLine("Enter an integer: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            obj.Method1(input);
+            obj.Method1();
+            obj.Method2(out int x, out int y);
+            Console.WriteLine("output parameters: {0}, {1}", x, y);
+            Console.WriteLine("\n");
+            StaticClass.SMethod();
+            Console.ReadLine();
+        }
+    }
+
+    static class StaticClass
+    {
+        public static void SMethod()
+        {
+            Console.WriteLine("This is my static class");
+        }
+    }
+}
