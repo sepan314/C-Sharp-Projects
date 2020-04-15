893c3415bc61a55a56684f596be090f80eb65789
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-15 16:40:33 -0400

    assignment14

---
+++ a14/a14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace a14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoParameters result = new TwoParameters();
+            Console.WriteLine("Enter an integer: ");
+            int int1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an integer (optional): ");
+            string int2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(int2))
+            {
+                Console.WriteLine(result.Multiply(int1));
+            }
+            else
+            {
+                Console.WriteLine(result.Multiply(int1, Convert.ToInt32(int2)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ a14/a14/TwoParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace a14
+{
+    public class TwoParameters
+    {
+        public int Multiply(int int1, int int2 = 10)
+        {
+            return int1 * int2;
+        }
+
+    }
+}
