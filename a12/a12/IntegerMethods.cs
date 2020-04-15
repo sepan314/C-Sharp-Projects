62e529777d851e6dce42cd959efc944de66654b8
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-15 15:34:28 -0400

    assignment12

---
+++ a12/a12/IntegerMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace a12
+{
+    public class IntegerMethods
+    {
+        public static int Add1000(int integer)
+        {
+            return integer += 1000;
+        }
+
+        public static int Multiply3(int integer)
+        {
+            return integer *= 3;
+        }
+
+        public static int Square(int integer)
+        {
+            return integer *= integer;
+        }
+    }
+}
---
+++ a12/a12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace a12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add1000: " + IntegerMethods.Add1000(input));
+            Console.WriteLine("Multiply3: " + IntegerMethods.Multiply3(input));
+            Console.WriteLine("Square: " + IntegerMethods.Square(input));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
