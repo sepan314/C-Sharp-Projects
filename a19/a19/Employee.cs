07aac2b8adb9c4581a8ec0fcf7cd357f140c2bb5
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-17 20:23:00 -0400

    assignment19

---
+++ a19/a19/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace a19
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+    }
+}
--- a19/a19/Program.cs
+++ a19/a19/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace a19
 {
@@ -6,7 +7,26 @@ namespace a19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee<string> e1 = new Employee<string>();
+            e1.Things = new List<string>() { "pen", "paper", "keys" };
+
+            Employee<int> e2 = new Employee<int>
+            {
+                Things = new List<int>() { 1, 3, 5, 7 }
+            };
+
+            foreach (string item in e1.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in e2.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
