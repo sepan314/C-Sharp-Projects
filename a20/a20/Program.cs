7c0c4c45820e27cbba974f28780bd42d44bd82ff
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-17 21:02:12 -0400

    add assignment for enum

---
+++ a20/a20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace a20
+{
+    class Program
+    {
+
+        enum Week
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week: ");
+            string input = Console.ReadLine();
+            try
+            {
+                Week myEnum = (Week)Enum.Parse(typeof(Week), input);
+                Console.WriteLine((int)myEnum);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
