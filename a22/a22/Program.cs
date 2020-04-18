c626eafc044a3b317d325bdc6edb8e985dafc4eb
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-18 00:17:27 -0400

    assignment for file io

---
+++ a22/a22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace a22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+
+            string filePath = @"/Users/sp/Desktop/C-Sharp-Projects/a22/a22/log.txt";
+
+
+            File.WriteAllText(filePath, input);
+
+            Console.WriteLine(File.ReadAllText(filePath));
+
+            Console.ReadLine();
+        }
+    }
+}
