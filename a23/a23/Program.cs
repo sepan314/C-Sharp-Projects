44225cda0ae863bdeb3ac42dca869cc124e37e14
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-18 00:31:18 -0400

    assignment for datetime

---
+++ a23/a23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace a23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            Console.WriteLine("Enter a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(now.AddHours(hours));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
