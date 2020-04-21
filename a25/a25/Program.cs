91a3cb368f5daf0f1c54516dfe0ed17c3d9f87a6
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-20 23:57:09 -0400

    assignment for exception handling pt 2

---
+++ a25/a25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace a25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age: ");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                int DOByear = 2020 - age;
+                Console.WriteLine("The year you were born in: " + DOByear);
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter a valid age.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input error: integers only");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
