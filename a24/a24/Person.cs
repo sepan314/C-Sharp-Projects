394822165a62f16a9b89c7216c06f929468a9cb2
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-04-20 23:44:56 -0400

    assignment for constructor chain

---
+++ a24/a24/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace a24
+{
+    public class Person
+    {
+        public const string species = "Homo sapiens";
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person() : this("John", "Doe")
+        {
+        }
+
+        public Person(string FName, string LName)
+        {
+            FirstName = FName;
+            LastName = LName;
+        }
+
+    }
+}
---
+++ a24/a24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace a24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Person1 = new Person();
+            Console.WriteLine("Person 1: {0} {1}, {2}", Person1.FirstName, Person1.LastName, Person.species);
+            Console.WriteLine($"Person 1 Type: {Person1.GetType()}");
+
+            Person Person2 = new Person("Jane", "Smith");
+            Console.WriteLine("Person 2: {0} {1}, {2}", Person2.FirstName, Person2.LastName, Person.species);
+            Console.WriteLine($"Person 2 Type: {Person2.GetType()}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
